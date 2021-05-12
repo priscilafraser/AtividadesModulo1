31624f4ed9204c8f65bf176529f0ff9bcf98fc3d
priscilafraser <priscila.fraser@gmail.com>
2021-05-12 18:24:39 -0400

    nova atualização

--- Exercicio 11.05.2021/exercicio4.py
+++ Exercicio 11.05.2021/exercicio4.py
@@ -3,14 +3,49 @@ parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indica
 pessoa tem voto NEGADO, OPCIONAL ou OBRIGATORIO nas eleições. Para resolver esse
 exercício, pesquise sobre a função date da biblioteca Datetime. """
 
-import datetime
+""" from datetime import date
 
 def voto(anoNasc, anoAtual):
     idade = anoAtual - anoNasc
-    if idade >18:
-        print('oi')
+    if idade >=18:
+        print('Obrigatório')
+    elif idade >=16 or idade <18:
+        print('Opcional')
+    else:
+        print('Negado')
 
 
 a= int(input('nascimento: '))
 b= int(input('atual: '))
-voto(a, b)
+voto(a, b) """
+
+
+""" from datetime import date
+
+def voto(y):
+    anoAtual = date.today().year
+    idade = anoAtual - y
+    if idade >= 18 and idade <= 70:
+        print('VOTO OBRIGATORIO')
+    elif idade >= 70:         #or idade >= 16 or idade < 18:
+        print('VOTO OPCIONAL')
+    else:
+        print('Negado') """
+
+
+
+from datetime import date
+
+def voto(y):
+    anoAtual = date.today().year
+    idade = anoAtual - y
+    if idade >= 18 and idade <= 70:
+        print('VOTO OBRIGATORIO')
+    elif idade >= 70:         #or idade >= 16 or idade < 18:
+        print('VOTO OPCIONAL')
+    else:
+        print('Negado')
+
+
+anoNascimento = int(input('Em que ano você nasceu?: '))
+voto(anoNascimento)
---
+++ Exercicio 11.05.2021/exercicio6.py
@@ -0,0 +1,29 @@
+""" def ficha(jogador='Não informado', gols=-1):
+    print('-'*40)
+    print(f'{"Jogador":<20}{"Gols":<20}')
+    if gols == -1:
+        print(f'{jogador:<20}{"Não informado":<20}')
+    else:
+        print(f'{jogador:<20}{gols:<20}')
+
+ficha()
+ficha('ana')
+ficha(gols=5)
+ficha('marta', 3) """
+
+""" def notas(a,b,c):
+    med=(a+b+c)/3
+    print('a media das 3 notas')
+    lista=[a,b,c]
+    min(lista)
+    medaltas=((a+b+c)-(min(lista)))/2
+    max(lista)
+
+
+
+print('a nota mais alta{max(lista)}')
+print('a nota mais alta{max(lista)}')
+print('a nota mais alta{max(lista)}') """
+
+
+def calculadora(notas)
